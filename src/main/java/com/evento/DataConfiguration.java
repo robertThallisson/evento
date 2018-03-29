dbc804724618b3e2cb3c5261da3c5b1550bb10ce
robert <robertnegueba12@gmail.com>
2018-03-29 16:05:26 -0300

    a

--- src/main/java/com/evento/DataConfiguration.java
+++ src/main/java/com/evento/DataConfiguration.java
@@ -21,7 +21,7 @@ public class DataConfiguration {
 
 	private String url = "jdbc:postgresql://" + host + ":" + porta + "/";
 
-	private String urlH = "URLhpostgres://xiybjazidgkgvl:55b1edcb73c1a735455adcd7c93426b1573ac11ebbac8a38c21b873fdc945dfe@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dbao9o4qkaieoh";
+	private String urlH = "postgres://xiybjazidgkgvl:55b1edcb73c1a735455adcd7c93426b1573ac11ebbac8a38c21b873fdc945dfe@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dbao9o4qkaieoh";
 	private String senhaH = "xiybjazidgkgvl";
 	private String UsuarioH = "55b1edcb73c1a735455adcd7c93426b1573ac11ebbac8a38c21b873fdc945dfe";
 
